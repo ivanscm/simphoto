d2927d2a6f43c6462210536d949c0d922038107e
IvanSCM <ivanscm@gmail.com>
2021-06-05 20:57:47 +0700

    init app

---
+++ vendor/composer/installed.php
@@ -0,0 +1,213 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3a9abcd05f59269959edf2e4f75730546bc32c92',
+    'name' => 'nette/web-project',
+  ),
+  'versions' => 
+  array (
+    'latte/latte' => 
+    array (
+      'pretty_version' => 'v2.10.3',
+      'version' => '2.10.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dd2c58863e830c22a2c6b85af63f37b829d255df',
+    ),
+    'nette/application' => 
+    array (
+      'pretty_version' => 'v3.1.2',
+      'version' => '3.1.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f817a0b738a3190efe1e573a099d1a80797e156b',
+    ),
+    'nette/bootstrap' => 
+    array (
+      'pretty_version' => 'v3.1.1',
+      'version' => '3.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'efe6c30fc009451f59fe56f3b309eb85c48b2baf',
+    ),
+    'nette/caching' => 
+    array (
+      'pretty_version' => 'v3.1.1',
+      'version' => '3.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3e771c589dee414724be473c24ad16dae50c1960',
+    ),
+    'nette/component-model' => 
+    array (
+      'pretty_version' => 'v3.0.1',
+      'version' => '3.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '66409cf5507c77edb46ffa88cf6a92ff58395601',
+    ),
+    'nette/database' => 
+    array (
+      'pretty_version' => 'v3.1.1',
+      'version' => '3.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '17bc9b2b5a5cd57ab089bc1a5982bc6c3b3790e3',
+    ),
+    'nette/di' => 
+    array (
+      'pretty_version' => 'v3.0.8',
+      'version' => '3.0.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1a3210f0f1f971db8a6e970c716c1cebd28b7ab0',
+    ),
+    'nette/finder' => 
+    array (
+      'pretty_version' => 'v2.5.2',
+      'version' => '2.5.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4ad2c298eb8c687dd0e74ae84206a4186eeaed50',
+    ),
+    'nette/forms' => 
+    array (
+      'pretty_version' => 'v3.1.3',
+      'version' => '3.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bfbef39b1f07dc5cdfeb7dba4843ee086ca0c065',
+    ),
+    'nette/http' => 
+    array (
+      'pretty_version' => 'v3.1.1',
+      'version' => '3.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c903d0f0b793ed2045a442f338e756e1d3954c22',
+    ),
+    'nette/mail' => 
+    array (
+      'pretty_version' => 'v3.1.6',
+      'version' => '3.1.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd5035afcb733ff76b353b8a126a1a6fd560005de',
+    ),
+    'nette/neon' => 
+    array (
+      'pretty_version' => 'v3.2.2',
+      'version' => '3.2.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e4ca6f4669121ca6876b1d048c612480e39a28d5',
+    ),
+    'nette/php-generator' => 
+    array (
+      'pretty_version' => 'v3.5.3',
+      'version' => '3.5.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '119f01a7bd590469cb01b538f20a125a28853626',
+    ),
+    'nette/robot-loader' => 
+    array (
+      'pretty_version' => 'v3.4.0',
+      'version' => '3.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3973cf3970d1de7b30888fd10b92dac9e0c2fd82',
+    ),
+    'nette/routing' => 
+    array (
+      'pretty_version' => 'v3.0.2',
+      'version' => '3.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5532e7e3612e13def357f089c1a5c25793a16843',
+    ),
+    'nette/schema' => 
+    array (
+      'pretty_version' => 'v1.2.1',
+      'version' => '1.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f5ed39fc96358f922cedfd1e516f0dadf5d2be0d',
+    ),
+    'nette/security' => 
+    array (
+      'pretty_version' => 'v3.1.3',
+      'version' => '3.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '817ee98aad1f122f8f40b728c35e634086e1093d',
+    ),
+    'nette/tester' => 
+    array (
+      'pretty_version' => 'v2.4.0',
+      'version' => '2.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0988c33459b49bfd6c8d06e16f29de96eba341e7',
+    ),
+    'nette/utils' => 
+    array (
+      'pretty_version' => 'v3.2.2',
+      'version' => '3.2.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '967cfc4f9a1acd5f1058d76715a424c53343c20c',
+    ),
+    'nette/web-project' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3a9abcd05f59269959edf2e4f75730546bc32c92',
+    ),
+    'symfony/thanks' => 
+    array (
+      'pretty_version' => 'v1.2.10',
+      'version' => '1.2.10.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e9c4709560296acbd4fe9e12b8d57a925aa7eae8',
+    ),
+    'tracy/tracy' => 
+    array (
+      'pretty_version' => 'v2.8.3',
+      'version' => '2.8.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '342674bbf72365e8456de9855a8cd839ca695933',
+    ),
+  ),
+);
